c108927bad724069831682744694026ad1a6df6b
Andrey Novichenko <60508206+anovicenko74@users.noreply.github.com>
2025-05-20 18:58:50 +0200

    f6pro

--- src/consts.ts
+++ src/consts.ts
@@ -1,25 +1,24 @@
-export const SITE_TITLE = "Token";
+export const SITE_TITLE = "POCO BLOG";
 export const SITE_DESCRIPTION =
-  `Token Astro + Tailwind CSS starter featuring a sleek black-and-white terminal-inspired design, with support for RSS, sitemap, MD/MDX blog posts static content.`.trim();
+  `Свежие новости о продуктах POCO. Держим руку на пульсе`.trim();
 
 export const KNOWN_TECH =
-  `Astro,Tailwindcss,Svelte,Nextjs,Javascript,Typescript,Python,SCSS,CSS,HTML,HTMX,Astro,Tailwindcss,Svelte,Nextjs,Javascript,Typescript,Python,SCSS,CSS,HTML,HTMX`.split(
+  `POCO`.split(
     ",",
   );
 export const ABOUT_ME =
-  `Token is a modern and minimalistic Astro + Tailwind CSS starter designed for developers, students, and creators looking to build clean, fast, and functional websites. Featuring a sleek black-and-white theme with a custom terminal-inspired font, this template is perfect for personal portfolios, blogs, or any static content-driven website. The template comes packed with features like Markdown (MD) and MDX support for writing blog posts or documentation, allowing for flexibility and ease of use. It includes Tailwind CSS for effortless styling and customization, enabling users to tweak the design to match their branding or personal preferences without hassle. Additionally, Token integrates essential utilities like RSS feed generation, a sitemap for SEO optimization, and pre-configured components for seamless content management. Everything is static, making the site incredibly fast, lightweight, and easy to deploy on platforms like Vercel, Netlify, or Deno Deploy. Whether you're a programmer showcasing your projects, a writer sharing your ideas, or someone looking to establish an online presence, this template provides all the tools you need in a stylish and functional package. With its developer-first approach and clean codebase, Token offers a strong foundation for creating visually appealing websites that stand out while keeping performance and simplicity at the forefront.`.trim();
-export const GITHUB_USERNAME = "ArnavK-09";
-export const QUOTE = "Programmer, Gamer, Editor and Student";
+  `POCO — это бескомпромиссные смартфоны с топовыми характеристиками по доступной цене. От игровых монстров (серия POCO F) до бюджетных «убийц» (серия POCO X и POCO M) — мы расскажем, какой гаджет стоит ваших денег.
+✨ Подпишитесь, чтобы не пропустить:
+    Свежие статьи — 2 раза в неделю.
+    Спецпредложения — скидки, промокоды и подарки.
+    Голосования — решайте, какой телефон мы разберём в следующем обзоре!
+    POCO Blog — для тех, кто любит технологии без переплат! 🚀`.trim();
+export const GITHUB_USERNAME = "unknown";
+export const QUOTE = "Свежие новости о продуктах POCO";
 export const NAV_LINKS: Array<{ title: string; href?: string }> = [
   {
-    title: "Blog",
+    title: "Новости",
+    href: 'blog'
   },
-  {
-    title: "Github",
-    href: "//github.com/" + GITHUB_USERNAME,
-  },
-  {
-    title: "Source",
-    href: "//github.com/ArnavK-09/token-template",
-  },
+  
 ];
